059a9db3ac0455e786d77706ab181b09abe3e592
leguan <leguanYa@gmail.com>
2023-06-19 18:12:36 +0800

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @Description：
+ * @Author：ZhangHui
+ * @Package：org.example
+ * @Date: ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
